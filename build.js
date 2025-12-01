fc437f6719f9ed0f7f6e259e3ab8975455ba198c
Danny Spencer <dan.spencer.np@gmail.com>
2025-12-01 11:57:01 -0800

    Set up GitHub workflow to create releases

--- build.js
+++ build.js
@@ -2,19 +2,27 @@ const esbuild = require('esbuild');
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-const gitHash = execSync('git rev-parse --short HEAD').toString().trim();
+let version;
 
-// Prefix with the current date:
-// YYMMDD-hash
-const date = new Date();
-const year = date.getFullYear().toString().slice(-2);
-const month = String(date.getMonth() + 1).padStart(2, '0');
-const day = String(date.getDate()).padStart(2, '0');
-const yymmdd = `${year}${month}${day}`;
-const version = `${yymmdd}-${gitHash}`;
+if (process.argv[2]) {
+    // Use the first argument as version if provided
+    version = process.argv[2];
+} else {
+    // Prefix with the current date:
+    // YYMMDD-hash
+    const date = new Date();
+    const year = date.getFullYear().toString().slice(-2);
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const yymmdd = `${year}${month}${day}`;
+
+    const gitHash = execSync('git rev-parse --short HEAD').toString().trim();
+
+    version = `${yymmdd}-${gitHash}`;
+}
 
 const metablock = `// ==UserScript== 
-// @name         Hide LinkedIn Posts
+// @name         LinkedIn Post Hider
 // @namespace    http://tampermonkey.net/
 // @version      ${version}
 // @description  Hide LinkedIn posts and other elements matching regex patterns
@@ -23,15 +31,17 @@ const metablock = `// ==UserScript==
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_registerMenuCommand
+// @updateURL    https://github.com/nukep/linkedin-post-hider/releases/latest/download/linkedin_post_hider.user.js
+// @downloadURL  https://github.com/nukep/linkedin-post-hider/releases/latest/download/linkedin_post_hider.user.js
 // @run-at       document-start
 // ==/UserScript==
 
 `;
 
 esbuild.build({
-  entryPoints: ['src/main.ts'],
-  bundle: true,
-  outfile: 'dist/linkedin_userscript.user.js',
-  format: 'iife',
-  banner: { js: metablock }
+    entryPoints: ['src/main.ts'],
+    bundle: true,
+    outfile: 'dist/linkedin_post_hider.user.js',
+    format: 'iife',
+    banner: { js: metablock }
 }).catch(() => process.exit(1));
